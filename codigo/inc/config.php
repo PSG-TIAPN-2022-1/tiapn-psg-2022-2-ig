fdea5782c9ef27563d5ba8da869f03a9631be6cb
Davi Perrier <perrier.davi@gmail.com>
2022-09-27 17:21:57 -0300

    adição de arquivos php

---
+++ codigo/inc/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+$link = "http://localhost/tiapn-psg-2022-2-ig/codigo/";
+
+/*
+Remover comentário quando houver BD
+$config_host = "localhost";
+$config_user = "";
+$config_pass = "";
+$config_sql = "";
+session_start();
+date_default_timezone_set("America/Sao_Paulo");
+$pdo = new PDO("mysql:host=".$config_host.";dbname=".$config_sql.";charset=UTF8;","".$config_user."","".$config_pass."");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+*/
+
+?>
